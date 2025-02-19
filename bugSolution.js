167a21e1ac1bd6f56a353aca02c3b050fd57bde8
BugHunter-X <askzenai@gmail.com>
2025-02-19 19:30:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,12 @@
+```javascript
+// pages/index.js
+export default async function Home() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const data = await res.json();
+  return (
+    <div>
+      <h1>{data.title}</h1>
+    </div>
+  );
+}
+```
